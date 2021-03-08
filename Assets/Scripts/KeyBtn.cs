3e8e47708c2a07d20eae3da1a31effef0ad2cf0d
Grégoire Lebas <gregoire.lebas@gmail.com>
2021-03-08 14:13:25 +0100

    Created a key panel for pin code.

---
+++ Assets/Scripts/KeyBtn.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class KeyBtn : MonoBehaviour
+{
+	public int Value { get; private set; } = 0;
+
+	[SerializeField] private TextMeshProUGUI valueText = null;
+
+	public void SetValue(int newValue)
+	{
+		Value = newValue;
+
+		valueText.text = Value.ToString();
+	}	
+}
---
+++ Assets/Scripts/KeyPanel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class KeyPanel : MonoBehaviour
+{
+	[SerializeField] private Transform keyContainer = null;
+
+	private KeyBtn[] keys = null;
+
+	private void Awake()
+	{
+		keys = keyContainer.GetComponentsInChildren<KeyBtn>();
+
+		List<int> keyValues = new List<int>();
+		for (int i = 0; i < keys.Length; i++)
+		{
+			keyValues.Add(i + 1);
+		}
+
+		System.Random rnd = new System.Random();
+		keyValues = keyValues.OrderBy(x => rnd.Next()).ToList();
+
+		for (int i = 0; i < keys.Length; i++)
+		{
+			keys[i].SetValue(keyValues[i]);
+		}
+	}
+}
